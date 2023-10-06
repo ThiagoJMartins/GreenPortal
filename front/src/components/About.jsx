a3389f586782a0110eb98d35c63511d99e9eedbf
are3s <thiagodev.martins@gmail.com>
2023-10-06 14:00:31 -0300

    aboutUpdate

--- front/src/components/About.jsx
+++ front/src/components/About.jsx
@@ -6,8 +6,13 @@ function About() {
     return (
         <div>
             <h1>
-                Hola, soy el About
+                About Us
             </h1>
+            <br />
+            <h2>We are <b>Ares WebDev</b></h2> 
+            <h3>We created GreenPortal, in order to obtain a kind of encyclopedia based on the television series <b>Rick and Morty</b><br />We hope you to enjoy this website</h3>
+            <br />
+            <img src="../../public/about.gif" alt="aboutGif" />
         </div>
     )
 }
