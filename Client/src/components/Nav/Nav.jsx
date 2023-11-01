e932e4d7794151ab913781c4e54743c9e8aad81e
Ares <thiagodev.martins@gmail.com>
2023-11-01 20:17:58 -0300

    Styles Nav & Card Update

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -6,43 +6,43 @@ import styles from "./Nav.module.scss";
 //!----------------------------------------------------+/
 
 export default function Nav(props) {
-  return (
-    <div className={styles.navContainer}>
-      <div className={styles.logo}>
-        <Link to={"/home"}>
-          <img src={Logo} alt="logo" width={200} />
-        </Link>
-      </div>
+	return (
+		<div className={styles.navContainer}>
+			<div className={styles.logo}>
+				<Link to={"/home"}>
+					<img src={Logo} alt="logo" width={200} />
+				</Link>
+			</div>
 
-      <div className={styles.secondSection}>
-        <div className={styles.wrapperItems2}>
-          <span onClick={props.logout} className={styles.itemNav} id="logout">
-            Logout
-          </span>
-          <Link to={"/about"} className={styles.linkNav}>
-            <span className={styles.itemNav} id="about">
-              About
-            </span>
-          </Link>
-        </div>
-        <div className={styles.wrapperItems1}>
-          <Link to={"/home"} className={styles.linkNav}>
-            <span className={styles.itemNav} id="home">
-              Home
-            </span>
-          </Link>
+			<div className={styles.secondSection}>
+				<div className={styles.wrapperItems1}>
+					<Link to={"/home"} className={styles.linkNav}>
+						<span className={styles.itemNav} id="home">
+							Home
+						</span>
+					</Link>
 
-          <Link to={"/favourites"} className={styles.linkNav}>
-            <span className={styles.itemNav} id="logout">
-              Favs
-            </span>
-          </Link>
-        </div>
+					<Link to={"/favourites"} className={styles.linkNav}>
+						<span className={styles.itemNav} id="logout">
+							Favs
+						</span>
+					</Link>
+				</div>
 
-        <div className={styles.searchNav}>
-          <SearchBar onSearch={props.onSearch} />
-        </div>
-      </div>
-    </div>
-  );
+				<div className={styles.searchNav}>
+					<SearchBar onSearch={props.onSearch} />
+				</div>
+				<div className={styles.wrapperItems2}>
+					<Link to={"/about"} className={styles.linkNav}>
+						<span className={styles.itemNav} id="about">
+							About
+						</span>
+					</Link>
+					<span onClick={props.logout} className={styles.itemNav} id="logout">
+						Logout
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 }
