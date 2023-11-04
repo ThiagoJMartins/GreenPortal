f9d97587ff26e977861d32b76e6a0c36008be571
Ares <thiagodev.martins@gmail.com>
2023-11-03 21:53:06 -0300

    M3 HW 2

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,73 +14,73 @@ import Favourites from "./components/Favourites/Favourites";
 //!----------------------------------------------------+/
 
 function App() {
-  const APIKEY = "pi-thiagojmartins";
-  const EMAIL = "thiagodev.martins@gmail.com";
-  const PASSWORD = "Thiago228";
+	// const APIKEY = "pi-thiagojmartins";
+	const EMAIL = "thiagodev.martins@gmail.com";
+	const PASSWORD = "Thiago228";
 
-  const [access, setAccess] = useState(false);
-  const [characters, setCharacters] = useState([]);
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+	const [access, setAccess] = useState(false);
+	const [characters, setCharacters] = useState([]);
+	const { pathname } = useLocation();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-  const onSearch = (id) => {
-    axios(`https://rym2.up.railway.app/api/character/${id}?key=${APIKEY}`)
-      .then(({ data }) => {
-        if (data.name) {
-          const characterExists = characters.filter(
-            (char) => char.id === data.id
-          );
-          if (characterExists.length === 0)
-            setCharacters((oldChars) => [...oldChars, data]);
-          else window.alert("This character has already been added");
-        } else {
-          window.alert("Not character found with this ID");
-        }
-      })
-      .catch((error) => window.alert(error.message));
-  };
+	const onSearch = (id) => {
+		axios(`http://localhost:3001/rickandmorty/character/${id}`)
+			.then(({ data }) => {
+				if (data.name) {
+					const characterExists = characters.filter(
+						(char) => char.id === data.id
+					);
+					if (characterExists.length === 0)
+						setCharacters((oldChars) => [...oldChars, data]);
+					else window.alert("This character has already been added");
+				} else {
+					window.alert("Not character found with this ID");
+				}
+			})
+			.catch((error) => window.alert(error.message));
+	};
 
-  const onClose = (id) => {
-    dispatch(removeFav(id));
-    setCharacters(characters.filter((char) => char.id !== Number(id)));
-  };
+	const onClose = (id) => {
+		dispatch(removeFav(id));
+		setCharacters(characters.filter((char) => char.id !== Number(id)));
+	};
 
-  function login(userData) {
-    if (userData.email === EMAIL && userData.password === PASSWORD) {
-      setAccess(true);
-      navigate("/home");
-    } else {
-      alert("Incorrect email or password, verify your data");
-    }
-  }
+	function login(userData) {
+		if (userData.email === EMAIL && userData.password === PASSWORD) {
+			setAccess(true);
+			navigate("/home");
+		} else {
+			alert("Incorrect email or password, verify your data");
+		}
+	}
 
-  function logout(event) {
-    setAccess(false);
-    navigate("/");
-  }
+	function logout(event) {
+		setAccess(false);
+		navigate("/");
+	}
 
-  useMemo(() => {
-    !access && navigate("/");
-  }, [access]);
+	useMemo(() => {
+		!access && navigate("/");
+	}, [access]);
 
-  return (
-    <div className="App">
-      {pathname !== "/" && <Nav onSearch={onSearch} logout={logout} />}
+	return (
+		<div className="App">
+			{pathname !== "/" && <Nav onSearch={onSearch} logout={logout} />}
 
-      <Routes>
-        <Route
-          path="/home"
-          element={<Cards characters={characters} onClose={onClose} />}
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/favourites" element={<Favourites />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="*" element={<Error />} />
-        <Route path="/" element={<Form login={login} />} />
-      </Routes>
-    </div>
-  );
+			<Routes>
+				<Route
+					path="/home"
+					element={<Cards characters={characters} onClose={onClose} />}
+				/>
+				<Route path="/about" element={<About />} />
+				<Route path="/favourites" element={<Favourites />} />
+				<Route path="/detail/:id" element={<Detail />} />
+				<Route path="*" element={<Error />} />
+				<Route path="/" element={<Form login={login} />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,1 +1,23 @@
+const http = require("http");
+const characters = require("./utils/data");
+//!----------------------------------------------------+/
+const PORT = 3001;
 
+http
+	.createServer((req, res) => {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+
+		const { url } = req;
+
+		if (url.includes("/rickandmorty/character")) {
+			const id = Number(url.split("/").at(-1));
+
+			const character = characters.find((char) => {
+				return char.id === id;
+			});
+
+			res.writeHead(200, { "Content-Type": "application/json" });
+			return res.end(JSON.stringify(character));
+		}
+	})
+	.listen(PORT, "localhost");
