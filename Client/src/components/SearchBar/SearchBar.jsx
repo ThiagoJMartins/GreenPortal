dbbab2a71966af18bb1a939a648d61eebce8ae66
ThiagoJMartins <thiagodev.martins@gmail.com>
2023-11-25 14:49:53 -0300

    Styles Hovers Update

--- Client/src/components/SearchBar/SearchBar.jsx
+++ Client/src/components/SearchBar/SearchBar.jsx
@@ -4,39 +4,39 @@ import styles from "./SearchBar.module.scss";
 //!----------------------------------------------------+/
 
 export default function SearchBar(props) {
-	const [id, setId] = useState("");
+  const [id, setId] = useState("");
 
-	const handleChange = (event) => {
-		const eventId = event.target.value;
-		setId(eventId);
-	};
+  const handleChange = (event) => {
+    const eventId = event.target.value;
+    setId(eventId);
+  };
 
-	const handleSearch = (id) => {
-		props.onSearch(id);
-		setId("");
-	};
+  const handleSearch = (id) => {
+    props.onSearch(id);
+    setId("");
+  };
 
-	const handleKeyDown = (event) => {
-		if (event.key === "Enter") {
-			handleSearch(id);
-		}
-	};
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSearch(id);
+    }
+  };
 
-	return (
-		<div className={styles.searchbar}>
-			<input
-				className="searchbar-input"
-				type="search"
-				value={id}
-				size="30"
-				placeholder="Insert ID"
-				onChange={handleChange}
-				onKeyDown={handleKeyDown}
-			/>
+  return (
+    <div className={styles.searchbar}>
+      <input
+        className="searchbar-input"
+        type="search"
+        value={id}
+        size="30"
+        placeholder="Insert ID"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
 
-			<button className={styles.searchbutton} onClick={() => handleSearch(id)}>
-				🔍
-			</button>
-		</div>
-	);
+      <button className={styles.searchbutton} onClick={() => handleSearch(id)}>
+        🔍
+      </button>
+    </div>
+  );
 }
