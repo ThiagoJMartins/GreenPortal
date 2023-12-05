dacec7cb9929ca6389127a9fe5a17c35bb066132
Ares <thiagodev.martins@gmail.com>
2023-12-05 13:00:36 -0300

    Update

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -7,77 +7,77 @@ import styles from "./Card.module.scss";
 //!----------------------------------------------------+/
 
 export default function Card({
-  onClose,
-  name,
-  status,
-  species,
-  gender,
-  origin,
-  image,
-  id,
+	onClose,
+	name,
+	status,
+	species,
+	gender,
+	origin,
+	image,
+	id,
 }) {
-  const [isFav, setIsFav] = useState(false);
+	const [isFav, setIsFav] = useState(false);
 
-  const dispatch = useDispatch();
-  const myFavourites = useSelector((state) => state.myFavourites);
+	const dispatch = useDispatch();
+	const myFavourites = useSelector((state) => state.myFavourites);
 
-  const handleFavourite = () => {
-    const character = {
-      name,
-      status,
-      species,
-      gender,
-      origin,
-      image,
-      id,
-    };
+	const handleFavourite = () => {
+		const character = {
+			name,
+			status,
+			species,
+			gender,
+			origin,
+			image,
+			id,
+		};
 
-    if (isFav) {
-      setIsFav(false);
-      dispatch(removeFav(id));
-    } else {
-      setIsFav(true);
-      dispatch(addFav(character));
-    }
-  };
+		if (isFav) {
+			setIsFav(false);
+			dispatch(removeFav(id));
+		} else {
+			setIsFav(true);
+			dispatch(addFav(character));
+		}
+	};
 
-  useEffect(() => {
-    myFavourites?.forEach((fav) => {
-      if (fav.id === id) {
-        setIsFav(true);
-      }
-    });
-  }, [myFavourites]);
+	useEffect(() => {
+		myFavourites?.forEach((fav) => {
+			if (fav.id === id) {
+				setIsFav(true);
+			}
+		});
+	}, [myFavourites]);
 
-  return (
-    <div className={styles.tarjeta}>
-      <div className={styles.botones}>
-        {isFav ? (
-          <button className={styles.boton2} onClick={handleFavourite}>
-            ❤️
-          </button>
-        ) : (
-          <button className={styles.boton2} onClick={handleFavourite}>
-            🤍
-          </button>
-        )}
-        <button className={styles.boton} onClick={() => onClose(id)}>
-          X
-        </button>
-      </div>
-      <Link to={`/detail/${id}`}>
-        <img className={styles.image} src={image} alt={name} />
-      </Link>
-      <h2 className={styles.name}>
-        {name} | {id}{" "}
-      </h2>
+	return (
+		<div className={styles.tarjeta}>
+			<div className={styles.botones}>
+				{isFav ? (
+					<button className={styles.boton2} onClick={handleFavourite}>
+						❤️
+					</button>
+				) : (
+					<button className={styles.boton2} onClick={handleFavourite}>
+						🤍
+					</button>
+				)}
+				<button className={styles.boton} onClick={() => onClose(id)}>
+					X
+				</button>
+			</div>
+			<Link to={`/detail/${id}`}>
+				<img className={styles.image} src={image} alt={name} />
+			</Link>
+			<h2 className={styles.name}>
+				{name} | {id}{" "}
+			</h2>
 
-      <div className={styles.text}>
-        <h4>STATUS | {status}</h4>
-        <h4>SPECIES | {species}</h4>
-        <h4>GENDER | {gender}</h4>
-        <h4>ORIGIN | {origin}</h4>
-      </div>
-    </div>
-  );
+			<div className={styles.text}>
+				<h4>STATUS | {status}</h4>
+				<h4>SPECIES | {species}</h4>
+				<h4>GENDER | {gender}</h4>
+				<h4>ORIGIN | {origin}</h4>
+			</div>
+		</div>
+	);
 }
